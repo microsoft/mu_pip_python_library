3ac67000c731f38494e475d195f5d26adc653e84
Matthew Carlson <matthewfcarlson@gmail.com>
2018-12-14 12:09:50 -0800

    Fleshed out the ansihandler test

--- MuPythonLibrary/MuAnsiHandler_test.py
+++ MuPythonLibrary/MuAnsiHandler_test.py
@@ -11,16 +11,29 @@ except ImportError:
 
 class MuAnsiHandlerTest(unittest.TestCase):
 
+    # we are mainly looking for exception to be thrown
+
     def test_formatter(self):
-        formatter = ColoredFormatter()
-        record = logging.LogRecord("test", logging.DEBUG, "test_path", 0, "Test message")
+        formatter = ColoredFormatter("%(levelname)s - %(message)s")
+        record = logging.makeLogRecord({"name": "test", "level": logging.CRITICAL,
+                                        "path": "test_path", "lineno": 0, "msg": "Test message"})
         output = formatter.format(record)
         self.assertNotEquals(output, None)
+        CSI = '\033['
+        if CSI not in output:
+            self.fail("There was supposed to be a ANSI control code in that")
 
     def test_handler(self):
         stream = StringIO()
+        # make sure we set out handler to strip the control sequence
         handler = ColoredStreamHandler(stream, strip=True)
-        formatter = ColoredFormatter()
+        formatter = ColoredFormatter("%(levelname)s - %(message)s")
         handler.formatter = formatter
-        record = logging.LogRecord("test", logging.CRITICAL, "test_path", 0, "Test message")
+        record = logging.makeLogRecord({"name": "test", "level": logging.CRITICAL,
+                                        "path": "test_path", "lineno": 0, "msg": "Test message"})
         handler.handle(record)
+        # check for ANSI escape code in stream
+        CSI = '\033['
+        for line in stream.readlines():
+            if CSI in line:
+                self.fail("A control sequence was not stripped!")
